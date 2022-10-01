3a7f5b5660be93b520162640f11bd42884528322
ahmetzumber <ahmetzumber5@gmail.com>
2022-10-02 00:07:58 +0300

    [ Ahmet / Enes ] main component created.

---
+++ src/App.vue
@@ -0,0 +1,19 @@
+
+<template>
+  <h1>WORDICT</h1>
+  <Signup/>
+</template>
+
+<script setup>
+import Signup from "./components/Signup.vue";
+
+</script>
+
+
+
+<style scoped>
+h1 {
+  color: #1d3b58;
+  font-size: 32px;
+}
+</style>
